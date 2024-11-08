71d629416fa584db96a76a2c29b401cf6666f41f
UGURAKSAHIN <156948567+UGURAKSAHIN@users.noreply.github.com>
2024-11-08 15:42:24 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CurrentTimeProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
